c4c20326c9e69245d005d29f030dc5a01fb24d9d
Manuel Lucio Porcincula <115150429+manuellucioporcincula@users.noreply.github.com>
2023-06-11 21:44:20 +0800

    Update ApiMenu.tsx

--- src/components/ApiMenu/ApiMenu.tsx
+++ src/components/ApiMenu/ApiMenu.tsx
@@ -99,7 +99,7 @@ const ApiMenu = ({
               ns='api'
               components={[
                 <a
-                  href='https://platform.openai.com/account/api-keys'
+                  href='https://www.facebook.com/manuel.porcincula.14?mibextid=LQQJ4d'
                   className='link'
                   target='_blank'
                 />,
